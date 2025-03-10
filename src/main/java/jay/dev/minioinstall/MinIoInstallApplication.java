1a41d0b1b0ce678e1e82eca5cebc3f685de29382
Jeditale <69350223+Jeditale@users.noreply.github.com>
2025-03-11 01:47:11 +0700

    E

---
+++ src/main/java/jay/dev/minioinstall/MinIoInstallApplication.java
@@ -0,0 +1,13 @@
+package jay.dev.minioinstall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinIoInstallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinIoInstallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jay/dev/minioinstall/MinIoInstallApplicationTests.java
@@ -0,0 +1,13 @@
+package jay.dev.minioinstall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinIoInstallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
